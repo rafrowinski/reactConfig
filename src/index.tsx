bdf7069509d1cd068fa708f5f5114050d2a51444
rrowinski <rrowinskipl@gmail.com>
2020-09-08 21:14:58 +0200

    adding esModuleInterop

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>Helloworld React</h1>, document.getElementById('root'));
